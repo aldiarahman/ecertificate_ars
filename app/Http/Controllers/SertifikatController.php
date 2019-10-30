c61883295225d21039aa2ba586ad075c369bddfc
Aldia Rahman <aldiarahman8@gmail.com>
2019-10-30 16:56:36 +0700

    Update crud

---
+++ app/Http/Controllers/SertifikatController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\sertifikat;
+use Illuminate\Http\Request;
+
+class SertifikatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\sertifikat  $sertifikat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(sertifikat $sertifikat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\sertifikat  $sertifikat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(sertifikat $sertifikat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\sertifikat  $sertifikat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, sertifikat $sertifikat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\sertifikat  $sertifikat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(sertifikat $sertifikat)
+    {
+        //
+    }
+}
--- database/migrations/2019_10_30_094231_create_kegiatans_table.php
+++ database/migrations/2019_10_30_094231_create_kegiatans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoleUserTable extends Migration
+class CreateKegiatansTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreateRoleUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('role_user', function (Blueprint $table) {
+        Schema::create('kegiatans', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('role_id')->unsigned();
-            $table->bigInteger('user_id')->unsigned();
+            $table->string('kodekegiatan');
+            $table->string('namakegiatan');
+            $table->date('tanggalkegiatan');
+            $table->string('penyelenggara');
+            $table->string('file_template');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +31,6 @@ class CreateRoleUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('role_user');
+        Schema::dropIfExists('kegiatans');
     }
 }
